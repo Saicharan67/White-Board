16874bf22bbdb95d2ceda6967986b96b0ea9d85c
Saicharan67 <mahankalisaicharan@gmail.com>
2021-01-31 20:26:17 +0530

    improving code to add undo/redo option

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import  './style.css'
-
+import $ from 'jquery'
 const Board = props => {
     const Colors = ['black','blue','red','green','yellow']
     let canvas,ctx,features1
@@ -29,7 +29,97 @@ const Board = props => {
         window.addEventListener('resize',()=>{
             fixHeight(canvas)
         })
+
         
+            // var tool;
+            // var canvas = document.getElementsByClassName("canvas")[0];
+            // var ctx = canvas.getContext('2d');
+            
+            // var history = {
+            //   redo_list: [],
+            //   undo_list: [],
+            //   saveState: function(canvas, list, keep_redo) {
+            //     keep_redo = keep_redo || false;
+            //     if(!keep_redo) {
+            //       this.redo_list = [];
+            //     }
+                
+            //     (list || this.undo_list).push(canvas.toDataURL());   
+            //   },
+            //   undo: function(canvas, ctx) {
+            //     this.restoreState(canvas, ctx, this.undo_list, this.redo_list);
+            //   },
+            //   redo: function(canvas, ctx) {
+            //     this.restoreState(canvas, ctx, this.redo_list, this.undo_list);
+            //   },
+            //   restoreState: function(canvas, ctx,  pop, push) {
+            //     if(pop.length) {
+            //       this.saveState(canvas, push, true);
+            //       var restore_state = pop.pop();
+            //       var img = new Element('img', {'src':restore_state});
+            //       img.onload = function() {
+            //         ctx.clearRect(0, 0, 600, 400);
+            //         ctx.drawImage(img, 0, 0, 600, 400, 0, 0, 600, 400);  
+            //       }
+            //     }
+            //   }
+            // }
+            
+            // var pencil = {
+            //   options: {
+            //     stroke_color: ['00', '00', '00'],
+            //     dim: 4
+            //   },
+            //   init: function(canvas, ctx) {
+            //     this.canvas = canvas;
+            //     this.canvas_coords = this.canvas.getCoordinates();
+            //     this.ctx = ctx;
+            //     this.ctx.strokeColor = this.options.stroke_color;
+            //     this.drawing = false;
+            //     this.addCanvasEvents();
+            //   },
+            //   addCanvasEvents: function() {
+            //     this.canvas.addEvent('mousedown', this.start.bind(this));
+            //     this.canvas.addEvent('mousemove', this.stroke.bind(this));
+            //     this.canvas.addEvent('mouseup', this.stop.bind(this));
+            //     this.canvas.addEvent('mouseout', this.stop.bind(this));
+            //   },
+            //   start: function(evt) {
+            //     var x = evt.page.x - this.canvas_coords.left;
+            //     var y = evt.page.y - this.canvas_coords.top;
+            //     this.ctx.beginPath();
+            //     this.ctx.moveTo(x, y);
+            //     history.saveState(this.canvas);
+            //     this.drawing = true;
+            //   },
+            //   stroke: function(evt) {
+            //     if(this.drawing) {
+            //       var x = evt.page.x - this.canvas_coords.left;
+            //       var y = evt.page.y - this.canvas_coords.top;
+            //       this.ctx.lineTo(x, y);
+            //       this.ctx.stroke();
+                  
+            //     }
+            //   },
+            //   stop: function(evt) {
+            //     if(this.drawing) this.drawing = false;
+            //   }
+            // };
+            
+            // document.getElementById('pencil').addEventListener('click', function() {
+            //   pencil.init(canvas, ctx);
+            // });
+            
+            // document.getElementById('undo').addEventListener('click', function() {
+            //   history.undo(canvas, ctx);
+            // });
+            
+            // document.getElementById('redo').addEventListener('click', function() {
+            //   history.redo(canvas, ctx);
+            // });
+          
+            
+          
     })
     
     const startDrawing = (e) => {
@@ -152,8 +242,14 @@ const Board = props => {
                    <button className = 'eraser' onClick={erase}>
                        Earser
                    </button>
-                   <button className = 'eraser' onClick={pencil}>
+                   <button className = 'eraser' id="pencil" onClick={pencil}>
                       pencil
+                   </button>
+                   <button className = 'eraser' id="undo" >
+                      undo
+                   </button>
+                   <button className = 'eraser' id="redo">
+                      redo
                    </button>
                    <button className = 'eraser' onClick={rectangle}>
                       rectangle
@@ -164,6 +260,8 @@ const Board = props => {
                     <button className = 'eraser' onClick={Circle}>
                     Circle
                    </button>
+                   
+                   
                 </div>
                 <div className="penciloptions">
                  <div>
