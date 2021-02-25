4f5b50361b6b2b7d0b3ff72d4aae593f028946dc
Saicharan67 <mahankalisaicharan@gmail.com>
2021-02-25 20:51:02 +0530

    removed usage of useRef hook

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import  './style.css'
 import NewPages from '../Features/pages/Newpages.js'
 
